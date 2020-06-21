7c55905c3882e40d0d164b9b2cc1c0940cad3681
ShinYongSUng <newxm@naver.com>
2020-06-21 13:48:22 +0900

    Initial commit

---
+++ app/src/main/java/my/shin/numberbaseballgame_2020062/BaseActivity.kt
@@ -0,0 +1,12 @@
+package my.shin.numberbaseballgame_2020062
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
---
+++ app/src/main/java/my/shin/numberbaseballgame_2020062/MainActivity.kt
@@ -0,0 +1,23 @@
+package my.shin.numberbaseballgame_2020062
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupevents()
+        setvalues()
+    }
+
+
+    override fun setupevents() {
+
+    }
+
+    override fun setvalues() {
+    }
+
+}
